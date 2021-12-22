d33d9b772d04f64c42f017277273fc01a36df8f1
OkkyHaryanto <63486943+OkkyHaryanto@users.noreply.github.com>
2021-12-22 23:26:59 +0700

    first commit

---
+++ src/main/java/com/webservice12/webservice12/Webservice12Application.java
@@ -0,0 +1,13 @@
+package com.webservice12.webservice12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webservice12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webservice12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/webservice12/webservice12/Webservice12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice12.webservice12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webservice12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
